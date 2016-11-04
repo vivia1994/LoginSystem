ab27e95e2cb265464133c3d08c7d3d468a111db7
雷薇 <leiwei183@163.com>
2016-11-04 10:56:57 +0800

    11/4

--- LoginEmpty/Controllers/HomeController.cs
+++ LoginEmpty/Controllers/HomeController.cs
@@ -10,14 +10,18 @@ using LoginEmpty.Models;
 
 namespace LoginEmpty.Controllers
 {
-    [Authorize]         //这个控制器中的所有操作都允许注册用户访问，禁止匿名访问
+    [Authorize] //这个控制器中的所有操作都允许注册用户访问，禁止匿名访问
     public class HomeController : Controller
     {
         // GET: Home
-        [AllowAnonymous]        //允许匿名访问
+        [AllowAnonymous] //允许匿名访问
         public ActionResult Index()
         {
-            Session.RemoveAll();
+            //DatabaseContext articleDbContext = new DatabaseContext();
+            //List<ArticleInfo> userArticle = articleDbContext.ArticleInfoes.ToList();
+            //var re = from x in userArticle select x;
+            //ViewBag.UserArticles = userArticle;
+
             return View();
         }
 
@@ -31,11 +35,12 @@ namespace LoginEmpty.Controllers
             DbSqlQuery<User> result = userDbContext.Users.SqlQuery("Select * from Users where Name = @p0", name);
             if (!result.Any())
                 return RedirectToAction("Index");
-            if (result.First().Password != password)        //密码错误
+            if (result.First().Password != password) //密码错误
                 return Redirect("Index");
             Session.Add("userInfo", result.First());
             return Redirect("Info");
         }
+
         /// <summary>
         /// 退出登录
         /// </summary>
@@ -55,7 +60,7 @@ namespace LoginEmpty.Controllers
         /// <returns></returns>
         public ActionResult Info()
         {
-            User user = (User)Session["userInfo"];
+            User user = (User) Session["userInfo"];
             ViewBag.UserInfo = user;
 
             if (ViewBag.UserInfo == null)
@@ -68,13 +73,14 @@ namespace LoginEmpty.Controllers
                 DatabaseContext articleDbContext = new DatabaseContext();
                 List<ArticleInfo> userArticle = articleDbContext.ArticleInfoes.ToList();
                 ViewBag.UserArticles = userArticle;
-                ViewBag.count=userArticle.Count;
+                ViewBag.count = userArticle.Count;
                 return View();
             }
 
         }
+
         [AllowAnonymous]
-        public ActionResult Register(string name, string password, string sex,DateTime? time)
+        public ActionResult Register(string name, string password, string sex, DateTime? time)
         {
             //    DatabaseContext userDbContext = new DatabaseContext();
             //    var r = from s in userDbContext.Users select s.Name.Equals(name);
@@ -83,68 +89,68 @@ namespace LoginEmpty.Controllers
             //count =r.Count();
             if (ModelState.IsValid)
 
-          {
-              if (!name.IsEmpty() && !password.IsEmpty()) /*&& count <= 0*/
-              {
-                  DatabaseContext newUser = new DatabaseContext();
-                  newUser.Users.Add(new User()
-                  {
-                      Name = name,
-                      Password = password,
-                      Sex = sex,
-                      Time = DateTime.Now
-                  });
+            {
+                if (!name.IsEmpty() && !password.IsEmpty()) /*&& count <= 0*/
+                {
+                    DatabaseContext newUser = new DatabaseContext();
+                    newUser.Users.Add(new User()
+                    {
+                        Name = name,
+                        Password = password,
+                        Sex = sex,
+                        Time = DateTime.Now
+                    });
 
-                  //  ViewBag.successMessage = "Regist successfully!";
-                  newUser.SaveChanges();
-              }
-              //try
-              //{
-              //    newUser.SaveChanges();
-              //}
-              //catch (DbEntityValidationException ex)
-              //{
-              //    StringBuilder errors = new StringBuilder();
-              //    IEnumerable<DbEntityValidationResult> validationResult = ex.EntityValidationErrors;
-              //    foreach (DbEntityValidationResult result in validationResult)
-              //    {
-              //        ICollection<DbValidationError> validationError = result.ValidationErrors;
-              //        foreach (DbValidationError err in validationError)
-              //        {
-              //            errors.Append(err.PropertyName + ":" + err.ErrorMessage + "\r\n");
-              //        }
-              //    Console.WriteLine(errors.ToString());
-              //简写
-              //var validerr = ex.EntityValidationErrors.First().ValidationErrors.First();
-              //Console.WriteLine(validerr.PropertyName + ":" + validerr.ErrorMessage);
-              else
-              {
-                  //{
-                  //    if (count>0&&!name.IsEmpty())
-                  //    {
-                  ViewBag.errorMessage = "input wrong!";
-                  //}
-                  //else if(name.IsEmpty()||password.IsEmpty())
-                  //{
-                  //    ViewBag.errorMessage = "Name or password cannot be null!";
-                  //}
-                  return View();
-              }
-          }
+                    //  ViewBag.successMessage = "Regist successfully!";
+                    newUser.SaveChanges();
+                }
+                //try
+                //{
+                //    newUser.SaveChanges();
+                //}
+                //catch (DbEntityValidationException ex)
+                //{
+                //    StringBuilder errors = new StringBuilder();
+                //    IEnumerable<DbEntityValidationResult> validationResult = ex.EntityValidationErrors;
+                //    foreach (DbEntityValidationResult result in validationResult)
+                //    {
+                //        ICollection<DbValidationError> validationError = result.ValidationErrors;
+                //        foreach (DbValidationError err in validationError)
+                //        {
+                //            errors.Append(err.PropertyName + ":" + err.ErrorMessage + "\r\n");
+                //        }
+                //    Console.WriteLine(errors.ToString());
+                //简写
+                //var validerr = ex.EntityValidationErrors.First().ValidationErrors.First();
+                //Console.WriteLine(validerr.PropertyName + ":" + validerr.ErrorMessage);
+                else
+                {
+                    //{
+                    //    if (count>0&&!name.IsEmpty())
+                    //    {
+                    ViewBag.errorMessage = "input wrong!";
+                    //}
+                    //else if(name.IsEmpty()||password.IsEmpty())
+                    //{
+                    //    ViewBag.errorMessage = "Name or password cannot be null!";
+                    //}
+                    return View();
+                }
+            }
             return RedirectToAction("Index");
         }
 
-        public ActionResult CreateBlog(string artiName,string articontent )
+        public ActionResult CreateBlog(string artiName, string articontent)
         {
 
             //未登录
-            User user = (User)Session["userInfo"];
+            User user = (User) Session["userInfo"];
             ViewBag.UserInfo = user;
             if (ViewBag.UserInfo == null)
             {
                 return RedirectToAction("Index");
             }
-                else
+            else
             {
                 ViewBag.userName = user.Name;
                 User writerName = (User) Session["userInfo"];
@@ -166,33 +172,46 @@ namespace LoginEmpty.Controllers
                 }
 
             }
-            return RedirectToAction("Info");
+            return View();
         }
-        public ActionResult Editblog()
-        {
-            User user = (User) Session["UserInfo"];
-            ViewBag.userName = user;
-            if (ViewBag.userName == null)
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                if (ModelState.IsValid)
-                {
-                    
-                }
-                return View();
-            }
-        }
-        //public ActionResult Search(string q)
+
+        //public ActionResult Editblog(string id, string artiName, string articontent)
         //{
-        //    DatabaseContext userResultDatabaseContext = new DatabaseContext();
-        //    var user = userResultDatabaseContext.Users.Where(x => x.Name.Contains(q)).ToList();
-        //    ViewBag.user = user;
-        //    return PartialView(user);
-        //}
+        //    User user = (User) Session["UserInfo"];
+        //    ViewBag.userName = user;
+        //    if (ViewBag.userName == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            int id1 = 0;
+        //            int.TryParse(id, out id1);
+        //            DatabaseContext allArti = new DatabaseContext();
+        //            ArticleInfo article = allArti.ArticleInfoes.Where(p => p.ArticleInfoId == id1).SingleOrDefault();
+        //            article.Article = ViewBag.Article;
+        //            article.Content = ViewBag.Content;
+        //            article.Article= artiName;
+        //            article.Content = articontent;
+        //            allArti.SaveChanges();
+        //            //var re = from s in allArti.ArticleInfoes
+        //            //    where s.ArticleInfoId == id1
+        //            //         select s;
+        //            //re.Article = artiName;
+        //            //Content = articontent;
+        //        }
+        //        return View();
+        //    }
+            //public ActionResult Search(string q)
+            //{
+            //    DatabaseContext userResultDatabaseContext = new DatabaseContext();
+            //    var user = userResultDatabaseContext.Users.Where(x => x.Name.Contains(q)).ToList();
+            //    ViewBag.user = user;
+            //    return PartialView(user);
+            //}
 
 
+        }
     }
-}
