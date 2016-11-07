a0bfcd2103f8b0c3322e6a1a9efa50b0516631d9
雷薇 <leiwei183@163.com>
2016-11-07 16:20:33 +0800

    SignUp <br/>

---
+++ Vlog/Vlog/scripts/vlog.js
@@ -0,0 +1,38 @@
+﻿$('#signin').click(function () {
+    $('#waiting').css('display', 'block');
+    $('#signin').attr("disabled", true);
+    $.post('/User/LoginSubmit',
+         { name: $('#name').val(), password: $('#password').val() },
+        function (data) {
+            if(data.flag) {
+                window.location.href = '/Article/User';
+            } else {
+                $('#waiting').css('display', 'none');
+                $('#signin').attr("disabled", false);
+                $('#warnning').text(data.reason);
+                $('#warnning').css('display', 'block');
+            }
+        });
+});
+
+$('#signUp')
+    .click(function () {
+        $('#signUpWaiting').css('display', 'block');
+        $('#signUp').attr("disabled", true);
+        $.post('/User/SignUpSubmit',
+            {
+                name: $('#signUpName').val(),
+                password: $('#signUpPassword').val(),
+                sex: $('input[type="radio"][name="sex"]:checked').val()
+            },
+            function (data) {
+                if (data.flag) {
+                    window.location.href = '/Home/Index';
+                } else {
+                    $('#signUpWaiting').css('display', 'none');
+                    $('#signUp').attr("disabled", false);
+                    $('#signUpWarnning').text(data.reason);
+                    $('#signUpWarnning').css('display', 'block');
+                }
+            });
+    });
