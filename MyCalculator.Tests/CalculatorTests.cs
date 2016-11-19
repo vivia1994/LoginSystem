a794d65170eef3499b6f754cc2c05d71b936c7c4
Vivia Lei <leiwei183@163.com>
2016-11-19 19:32:27 +0800

    Organizing unit tests test attributes

--- MyCalculator.Tests/CalculatorTests.cs
+++ MyCalculator.Tests/CalculatorTests.cs
@@ -7,7 +7,17 @@ namespace MyCalculator.Tests
     [TestClass]
     public class CalculatorTests
     {
+        //- Description allows you to specify a description for the test. 
+        //- Owner specifies the owner of the test. 
+        //- HostType allows you to specify the type of host the test can run on. 
+        //- Priority specifies the priority at which this test should run. 
+        //- Timeout specifies how long the test may run until it times out. 
+        //- WorkItem allows you to specify the work item IDs the test is associated with. 
+        //- CssProjectStructure represents the node in the team project hierarchy to which this test corresponds. 
+        //- CssIteration represents the project iteration to which this test corresponds.
         [TestMethod]
+        [TestCategory("Add")]
+        [Ignore]
         public void AddSimple()
         {
             //Arrange: initialize the environment
@@ -19,6 +29,7 @@ namespace MyCalculator.Tests
         }
 
         [TestMethod]
+        [TestCategory("Divide")]
         public void DivideSimple()
         {
             //Arrange
@@ -32,6 +43,7 @@ namespace MyCalculator.Tests
         }
         [TestMethod]
         [ExpectedException(typeof(DivideByZeroException))]
+        [TestCategory("Divide")]
         public void DivideByZero()
         {
             Calculator calculator = new Calculator();
