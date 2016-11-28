14104d22773f40448c2ef7df063eaa2f157b5bb9
雷薇 <leiwei183@163.com>
2016-11-28 11:25:39 +0800

    table not complete

---
+++ ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ImageUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ImageDatabaseContext database;
+        public HomeController()
+        {
+            database = new ImageDatabaseContext();
+        }
+            // GET: Home
+            public ActionResult Index()
+        {
+            List<Image> images = database.Images.ToList();
+            ViewBag.Images = images;
+            return View();
+        }
+    }
+}
